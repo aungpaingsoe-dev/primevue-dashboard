5182642fa0a25a424a672cd531288c2f90505d5a
Aung Paing Soe <aungpaingsoe097238@gmail.com>
2023-09-20 10:57:45 +0630

    user data table

--- src/components/Dashboard/UserManagement/Users/List.vue
+++ src/components/Dashboard/UserManagement/Users/List.vue
@@ -1,3 +1,50 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+const users = ref([]);
+
+const getUsers = async () => {
+    await Http.get('/v1/user')
+        .then((response) => {
+            users.value = response.data;
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+};
+
+const handleEditUser = (user) => {
+    console.log(user);
+};
+
+const handleRejectUser = (user) => {
+    console.log(user);
+};
+
+onMounted(() => {
+    getUsers();
+});
+</script>
+
 <template>
-    <h1>User List</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <DataTable :value="users" paginator :rows="5" :rowsPerPageOptions="[5, 10, 20, 50]" tableStyle="min-width: 50rem">
+                    <Column field="id" sortable header="Id"></Column>
+                    <Column field="name" sortable header="Name"></Column>
+                    <Column field="email" sortable header="Email"></Column>
+                    <template #empty> No customers found. </template>
+                    <template #loading> Loading customers data. Please wait. </template>
+                    <Column header="Actions">
+                        <template #body="slotProps">
+                            <div class="btn-group btn-sm">
+                                <button class="btn btn-outline-dark" @click="handleEditUser(slotProps.data)">Edit</button>
+                                <button class="btn btn-outline-dark" @click="handleRejectUser(slotProps.data)">Reject</button>
+                            </div>
+                        </template>
+                    </Column>
+                </DataTable>
+            </div>
+        </div>
+    </div>
 </template>
